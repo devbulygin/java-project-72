8d0349092b5b03db220ab296f405a1113184bf07
whoiswho68rus@gmail.com <githubRovnKresRim4>
2023-02-16 01:14:58 +0400

    correct checkstyle

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -52,18 +52,18 @@ public class App {
         return app;
     }
 
-        private static TemplateEngine getTemplateEngine() {
-            TemplateEngine templateEngine = new TemplateEngine();
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
 
-            ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-            templateResolver.setPrefix("/templates/");
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
 
-            templateEngine.addTemplateResolver(templateResolver);
-            templateEngine.addDialect(new LayoutDialect());
-            templateEngine.addDialect(new Java8TimeDialect());
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
 
-            return templateEngine;
-        }
+        return templateEngine;
+    }
 
     private static void addRoutes(Javalin app) {
         app.get("/", RootController.welcome);
